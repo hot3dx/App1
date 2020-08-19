cac50bf9295c37a718adef60a2024cb5fc5ebe1a
hot3dx <hot3dx@hotmail.com>
2020-08-19 11:26:30 -0600

    Add files via upload
    Library Directory changes to C:\DirectXToolKitXaml12 to make life much easier

--- pch.h
+++ pch.h
@@ -39,34 +39,37 @@
 #endif
 
 #if defined(_WIN32) &&  defined(_DEBUG)
+//#pragma comment(lib, "C://Users//hot3dx-home//Source//Repos//Xaml//DirectXToolKitXaml12//Debug//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #pragma comment(lib, "C://DirectXToolKitXaml12//Debug//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #endif
 #if defined(NDEBUG) &&  defined(_WIN32)
+//#pragma comment(lib, "C://Users//hot3dx-home//Source//Repos//Xaml//DirectXToolKitXaml12//Release//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #pragma comment(lib, "C://DirectXToolKitXaml12//Release//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #endif
 
 #if defined(NDEBUG) &&  defined(_X64)
+//#pragma comment(lib, "C://Users//hot3dx-home//Source//Repos//Xaml//DirectXToolKitXaml12//x64//Release//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #pragma comment(lib, "C://DirectXToolKitXaml12//x64//Release//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #endif
 
 #if defined(_DEBUG) &&  defined(_X64)
+//#pragma comment(lib, "C://Users//hot3dx-home//Source//Repos//Xaml//DirectXToolKitXaml12//x64//Debug//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #pragma comment(lib, "C://DirectXToolKitXaml12//x64//Debug//DirectXToolKitXaml12//DirectXToolKitXaml12.lib")
 #endif
 
-
-#include "../DirectXToolKitXaml12/Audio/Audio.h"
-#include "../DirectXToolkitXaml12/Graphics/CommonStates.h"
-#include "../DirectXToolkitXaml12/Graphics/DirectXHelpers.h"
-#include "../DirectXToolkitXaml12/Graphics/DDSTextureLoader.h"
-#include "../DirectXToolkitXaml12/Graphics/DescriptorHeap.h"
-#include "../DirectXToolkitXaml12/Graphics/Effects.h"
-#include "../DirectXToolkitXaml12/Graphics/GeometricPrimitive.h"
-#include "../DirectXToolkitXaml12/Graphics/GraphicsMemory.h"
-#include "../DirectXToolkitXaml12/Graphics/Model.h"
-#include "../DirectXToolkitXaml12/Graphics/PrimitiveBatch.h"
-#include "../DirectXToolkitXaml12/Graphics/MyResourceUploadBatch.h"
-#include "../DirectXToolkitXaml12/Graphics/RenderTargetState.h"
-#include "../DirectXToolkitXaml12/Graphics/SimpleMath.h"
-#include "../DirectXToolkitXaml12/Graphics/SpriteBatch.h"
-#include "../DirectXToolkitXaml12/Graphics/SpriteFont.h" 
-#include "../DirectXToolkitXaml12/Graphics/VertexTypes.h"
+#include <Audio/Audio.h>
+#include <Graphics/CommonStates.h>
+#include <Graphics/DirectXHelpers.h>
+#include <Graphics/DDSTextureLoader.h>
+#include <Graphics/DescriptorHeap.h>
+#include <Graphics/Effects.h>
+#include <Graphics/GeometricPrimitive.h>
+#include <Graphics/GraphicsMemory.h>
+#include <Graphics/Model.h>
+#include <Graphics/PrimitiveBatch.h>
+#include <Graphics/MyResourceUploadBatch.h>
+#include <Graphics/RenderTargetState.h>
+#include <Graphics/SimpleMath.h>
+#include <Graphics/SpriteBatch.h>
+#include <Graphics/SpriteFont.h> 
+#include <Graphics/VertexTypes.h>
